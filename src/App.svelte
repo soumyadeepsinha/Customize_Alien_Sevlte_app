6c196bdd1637349191016f923417efb8dd7ae28d
soumyadeepsinha <s.sinha2701@gmail.com>
2021-09-23 12:43:29 +0530

    added hat

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,63 @@
 <script>
 	let hue = 0;
 	let brightness = 100;
+	let saturation = 100;
+	let hatSizes = 1;
 
 	$: aliencolor = `
-	filter: hue-rotate(${hue}deg) brightness(${brightness}%);`;
-</script>
+	filter: hue-rotate(${hue}deg)
+	brightness(${brightness}%)
+	saturate(${saturation}%);`;
 
+	$: hatStyle = `transform: translate(-55%, -35%) scale(${hatSizes})`;
+</script>
 <main>
-	<img style="{aliencolor}" src="./assets/alien.png" alt="Alien">
-	<input type="range" min="0" max="360" bind:value={hue}/>
-	<input type="range" min="50" max="150" bind:value={brightness}/>
+	<h1>Your Alien Agent</h1>
+	<div class="alien">
+		<img style="{aliencolor}" class="alien-body" src="./assets/alien.png" alt="Alien">
+		<img style="{hatStyle}" class="hat" src="./assets/hat-2.png" alt="Hat">
+	</div>
+	<br>
+	<label for="hue">Color</label>
+	<input type="range" min="0" max="360" bind:value={hue}/> <br />
+	<label for="brightness">Brightness</label>
+	<input type="range" min="50" max="150" bind:value={brightness}/> <br />
+	<label for="saturation">Saturation</label>
+	<input type="range" min="0" max="250" bind:value={saturation}/> <br />
+	<label for="hatsize">Hat Size</label>
+	<input type="range" step="0.05" min="0.3" max="1.5" bind:value={hatSizes}/>
 </main>
 
 <style>
+
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		overflow-x: hidden;
 	}
+
+	.alien {
+		margin-top: 50px;
+		position: relative;
+		width: 300px;
+	}
+
+	.alien-body {
+		width: 100%;
+		height: auto;
+	}
+
+	.hat {
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translate(-55%, -35%);
+	}
+
+	img{
+		image-rendering: crisp-edges;
+	}
+
 	</style>
